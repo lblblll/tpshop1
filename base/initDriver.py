f3772bd87533ccdf396d491342b1834cee079a6e
LBinglian <lydia_lam3829@163.com>
2018-10-25 21:00:05 +0800

    修改了用户是否第一次启动

--- base/initDriver.py
+++ base/initDriver.py
@@ -11,7 +11,7 @@ def initDriver():
     desired_caps["appActivity"] = "com.tpshop.malls.SplashActivity"
     desired_caps["resetKeyboard"] = True
     desired_caps["unicodeKeyboard"] = True
-    desired_caps["noReset"] = True
+    desired_caps["noReset"] = False
     desired_caps['automationName'] = 'Uiautomator2'
     driver = webdriver.Remote("http://localhost:4723/wd/hub", desired_caps)
 
